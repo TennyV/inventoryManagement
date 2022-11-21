99e798e0ad127915efe1f970e854582d84243ef7
TennyV <tenny.vongtip@gmail.com>
2022-11-21 16:05:54 -0500

    textview

--- app/src/main/java/com/snhu/inventorymanagement/MainActivity.java
+++ app/src/main/java/com/snhu/inventorymanagement/MainActivity.java
@@ -3,6 +3,11 @@ package com.snhu.inventorymanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView userName = findViewById(R.id.userName);
+        TextView passWord = findViewById(R.id.passWord);
+        MaterialButton loginButton = findViewById(R.id.loginButton);
+
+
+        loginButton.setOnClickListener(view -> {
+            if (userName.getText().toString().equals("admin") && passWord.getText().toString().equals("admin")) {
+
+                Toast.makeText(MainActivity.this, "Login Successful", Toast.LENGTH_SHORT).show();
+            } else
+
+                Toast.makeText(MainActivity.this,"Login Failed",Toast.LENGTH_SHORT).show();
+        });
     }
 }
