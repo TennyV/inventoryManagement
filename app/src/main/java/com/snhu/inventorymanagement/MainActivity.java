8dcb027c3d41b079094ee1b4bd4de9cb0bbc384e
TennyV <tenny.vongtip@gmail.com>
2022-11-21 20:27:53 -0500

    SQL start

--- app/src/main/java/com/snhu/inventorymanagement/MainActivity.java
+++ app/src/main/java/com/snhu/inventorymanagement/MainActivity.java
@@ -3,6 +3,8 @@ package com.snhu.inventorymanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -11,14 +13,35 @@ import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText userName = findViewById(R.id.userName);
+    EditText passWord = findViewById(R.id.passWord);
+    MaterialButton loginButton = findViewById(R.id.loginButton);
+    MaterialButton  newUserButton = findViewById(R.id.newUser);
+    DBHelper myDb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView userName = findViewById(R.id.userName);
-        TextView passWord = findViewById(R.id.passWord);
-        MaterialButton loginButton = findViewById(R.id.loginButton);
+        myDb = new DBHelper(this);
+
+        newUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String user = userName.getText().toString();
+                String pass = passWord.getText().toString();
+                if(user.equals("") || pass.equals(""))
+                {
+                    Toast.makeText(MainActivity.this, "Fill in all fields",
+                                    Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+
+                }
+            }
+        });
 
 
         loginButton.setOnClickListener(view -> {
